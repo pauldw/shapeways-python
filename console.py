51dc6a98f2f7f4d5854bbf2e400d750a96614dd1
pauldw <pwalker@fvml.ca>
2013-11-12 10:51:58 -0500

    Adding interactive mode for creating key file.

--- console.py
+++ console.py
@@ -1,23 +1,50 @@
+'''Console for experimenting with the Shapeways API.'''
 import shapeways
+import json
+import sys
+import os
 
-# Get keys from the environment
-try:
-    consumer_key = (os.environ['SW_CONSUMER_KEY'], os.environ['SW_CONSUMER_KEY_SECRET'])
-    access_token = (os.environ['SW_ACCESS_TOKEN'], os.environ['SW_ACCESS_TOKEN_SECRET'])
-except KeyError, e:
-    sys.stderr.write("Please set SW_CONSUMER_KEY, SW_CONSUMER_KEY_SECRET, SW_ACCESS_TOKEN and SW_ACCESS_TOKEN_SECRET environment variables.\n")
+def input(prompt):
+    sys.stdout.write(prompt + ": ")
+    return raw_input()
 
-def run_price():
-    session = shapeways.get_oauth_session()
-    return shapeways.get_price(session, 0.000001, 0.0008, (0, 0, 0), (0.001, 0.001, 0.001))
-
-def run_dance():
-    oa = shapeways.OAuthDance(consumer_key)
-    (request_token, url) = oa.get_request()
-    print url
-    print "Enter verifier:"
-    verifier = raw_input()
-    access_token = oa.get_access(request_token, verifier)
+def create_keys_interactive(filename):
+    '''Interactive portion to help with creating a persistent keyfile for future console use.'''    
+    print("It looks like you need to create some keys for the console.  Keys will be stored in %s." % filename)
+    print("Note you must first have created an app on Shapeways at https://developers.shapeways.com/manage-apps/create")
     
-    print "Access token:"
-    print access_token
+    consumer_key = input("consumer key")
+    consumer_key_secret = input ("consumer key secret")
+    oa = shapeways.OAuthDance((consumer_key, consumer_key_secret))
+    (request_token, url) = oa.get_request()
+    
+    print("Go to: %s" % url)
+    
+    verifier = input("verifier")
+    (access_token, access_token_secret) = oa.get_access(request_token, verifier)
+    
+    print("Access token: %s" % access_token)
+    print("Access token secret: %s" % access_token_secret)
+    
+    oauth_keys = {'consumer_key': consumer_key, 'consumer_key_secret': consumer_key_secret, 'access_token': access_token, 'access_token_secret': access_token_secret}
+    
+    json.dump(oauth_keys, open(filename, 'wb'))
+    
+    print "Wrote keys to %s.  Done!" % filename
+
+def get_keys(filename='console_oauth_keys.json'):
+    '''Returns keys stored in filename as ((consumer_key, consumer_key_secret), (access_token, access_token_secret))'''
+    if not os.path.isfile(filename):
+        create_keys_interactive(filename)
+
+    keys = json.load(open(filename))
+    consumer_key = keys['consumer_key']
+    access_token = keys['access_token']
+    
+    return (consumer_key, access_token)
+
+def get_api():
+    keys = get_keys()
+    return shapeways.API(keys[0], keys[1])
+
+api = get_api()
