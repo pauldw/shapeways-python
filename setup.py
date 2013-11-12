b18d058e4033065091ffeb8fb49fd5d5a4725f39
pauldw <pwalker@fvml.ca>
2013-11-11 20:41:13 -0500

    Version check added to setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from distutils.core import setup
+import sys
+
+if sys.version_info[0] == 2 and sys.version_info[1] < 7:
+    sys.exit(1)
 
 setup(name='shapeways',
-      version='2',
+      version='0.1.3',
       description='Shapeways python bindings',
       author='Paul Walker',
       author_email='pwalker@fvml.ca',
-      url='https://fvml.ca',
+      url='https://github.com/pauldw/shapeways-python',
       packages=['shapeways'],
-      install_requires=['rauth'],
+      install_requires=['rauth >= 0.6.2'],
 )
-
-# TODO: install_requires and test_suite
