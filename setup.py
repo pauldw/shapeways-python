a851e2447e2469ce614289b1574467bd2ea66456
Paul Walker <pwalker@fvml.ca>
2014-01-30 10:42:51 -0500

    Changed module and PyPI package name to shapeways2

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ import sys
 if sys.version_info[0] == 2 and sys.version_info[1] < 7:
     sys.exit(1)
 
-setup(name='shapeways',
-      version='0.1.7',
-      description='Shapeways python bindings',
+setup(name='shapeways2',
+      version='0.1.8',
+      description='Unofficial Shapeways python bindings',
       author='Paul Walker',
       author_email='pwalker@fvml.ca',
       url='https://github.com/pauldw/shapeways-python',
-      packages=['shapeways'],
+      packages=['shapeways2'],
       install_requires=['rauth >= 0.6.2'],
 )
